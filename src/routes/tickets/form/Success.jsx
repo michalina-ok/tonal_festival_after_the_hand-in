e9be3fb1df5c2b5c55bfa16d51aa5f665c6a9550
Michalina <michalina.oniszczuk@gmail.com>
2022-12-20 19:00:37 +0100

    added link to the app in Success.jsx

--- src/routes/tickets/form/Success.jsx
+++ src/routes/tickets/form/Success.jsx
@@ -6,7 +6,7 @@ function Success() {
       <h2>Order received</h2>
       <h3>We can't wait to party with you soon</h3>
       <h3>And in the meantime check our app here</h3>
-      <button className="round">Get the app</button>
+      <a href="https://tonal-festival-app.netlify.app/">Get the app</a>
     </div>
   )
 }
